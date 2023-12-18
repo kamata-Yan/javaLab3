0e3c6d1879102a875c5f7fe5217aa7306ed0ccc9
kamata-Yan <75886277+kamata-Yan@users.noreply.github.com>
2023-12-18 14:35:03 +0300

    First commit

---
+++ src/infa.java
@@ -0,0 +1,12 @@
+
+
+public class infa {
+    private final String surname = "Гребень";
+    private final int group = 5;
+
+    @Override
+    public String toString() {
+        return "Фамилия " + surname + " " +
+                ", Группа " + group;
+    }
+}
